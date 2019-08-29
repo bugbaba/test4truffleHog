9f1d1375c465b340a094469f669364a8fc0dcdcf
Noman <kyleslayer@protonmail.com>
2019-08-30 00:01:06 +0530

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -11,3 +11,4 @@ dev-internal.example.com
 api-secret = "aaaaaaaaaaaa"
 
 shodan_api = "jn7wbEGfJqMUZOKmA5k7RTJJNPAbtyTG"
+secret_sendgrid=SG.af7c6bMLRxSagExMsW8Hng.vaa_FWbgCcVdkXhDXXg23SWS4cT5RcRtClbcdIb2d3b
